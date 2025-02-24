c99ed17d457fa3b969e709d01a7df1ed06b44863
VasKuzn <vakuznetsov_2@edu.hse.ru>
2025-02-24 17:50:41 +0500

    add+refactor: добавлен форматтер, запущены линтер и форматтер

--- infrastructure/loginprojectvite/eslint.config.js
+++ infrastructure/loginprojectvite/eslint.config.js
@@ -1,13 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { Linter } from 'eslint';
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  {
+    files: ['src**/*.js'],
+    languageOptions: { sourceType: 'script' },
+  },
+
   pluginJs.configs.recommended,
+
   ...tseslint.configs.recommended,
 ];
